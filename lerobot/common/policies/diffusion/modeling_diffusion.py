020038a37ea0cdde293c49b4363760827513dd48
Haotian <hz2851@nyu.edu>
2025-10-20 03:56:11 -0400

    debug

--- lerobot/common/policies/diffusion/modeling_diffusion.py
+++ lerobot/common/policies/diffusion/modeling_diffusion.py
@@ -201,7 +201,7 @@ class DiffusionPolicy(PreTrainedPolicy):
             if self.config.use_single_channel_goal and "observation.images.agentview_goal_gripper_proj" in batch:
                 batch = repeat_goal_first_channel_as_rgb(batch, "observation.images.agentview_goal_gripper_proj")
             else:
-                batch["observation.images.agentview_goal_gripper_proj"] = batch["observation.images.agentview_goal_gripper_proj"][..., [1, 2, 0]] # top, left, right -> left, right, top
+                batch["observation.images.agentview_goal_gripper_proj"] = batch["observation.images.agentview_goal_gripper_proj"][:, :, [1, 2, 0], :, : ] # top, left, right -> left, right, top
             batch["observation.images"] = torch.stack(
                 [batch[key] for key in self.config.image_features], dim=-4
             )
@@ -233,7 +233,7 @@ class DiffusionPolicy(PreTrainedPolicy):
             if self.config.use_single_channel_goal:
                 batch = repeat_goal_first_channel_as_rgb(batch, "observation.images.agentview_goal_gripper_proj")
             else:
-                batch["observation.images.agentview_goal_gripper_proj"] = batch["observation.images.agentview_goal_gripper_proj"][..., [1, 2, 0]] # top, left, right -> left, right, top
+                batch["observation.images.agentview_goal_gripper_proj"] = batch["observation.images.agentview_goal_gripper_proj"][:, :, [1, 2, 0], :, : ] # top, left, right -> left, right, top
             batch["observation.images"] = torch.stack(
                 [batch[key] for key in self.config.image_features], dim=-4
             )
