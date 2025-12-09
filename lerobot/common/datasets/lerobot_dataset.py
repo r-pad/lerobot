working rollout; training rotation bug fixed (to be verified)

lerobot/common/datasets/lerobot_dataset.py
@@ -519,10 +519,18 @@ class LeRobotDataset(torch.utils.data.Dataset):
         self.episode_data_index = get_episode_data_index(self.meta.episodes, self.episodes)
 
         # Check timestamps
-        timestamps = torch.stack(self.hf_dataset["timestamp"]).numpy()
-        episode_indices = torch.stack(self.hf_dataset["episode_index"]).numpy()
+        # timestamps = torch.stack(self.hf_dataset["timestamp"]).numpy()
+        # episode_indices = torch.stack(self.hf_dataset["episode_index"]).numpy()
+        # ep_data_index_np = {k: t.numpy() for k, t in self.episode_data_index.items()}
+        # check_timestamps_sync(timestamps, episode_indices, ep_data_index_np, self.fps, self.tolerance_s)
+
+        # Check timestamps
+        timestamps = torch.stack(tuple(self.hf_dataset["timestamp"])).numpy()
+        episode_indices = torch.stack(tuple(self.hf_dataset["episode_index"])).numpy()
         ep_data_index_np = {k: t.numpy() for k, t in self.episode_data_index.items()}
         check_timestamps_sync(timestamps, episode_indices, ep_data_index_np, self.fps, self.tolerance_s)
+
+
 
         # Setup delta_indices
         if self.delta_timestamps is not None:

lerobot/common/robot_devices/control_utils.py
@@ -29,6 +29,8 @@ import rerun as rr
 import torch
 from deepdiff import DeepDiff
 from termcolor import colored
+from PIL import Image
+from termcolor import cprint
 
 from lerobot.common.datasets.image_writer import safe_stop_image_writer
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
@@ -38,6 +40,10 @@ from lerobot.common.robot_devices.robots.utils import Robot
 from lerobot.common.robot_devices.utils import busy_wait
 from lerobot.common.utils.utils import get_safe_torch_device, has_method
 from lerobot.common.utils.aloha_utils import ALOHA_CONFIGURATION, ALOHA_MODEL, VIRTUAL_CAMERA_MAPPING, forward_kinematics, render_and_overlay, setup_renderer
+from lerobot.scripts.yufei_policy_utils import compute_pcd, get_gripper_4_points_from_sriram_data, \
+    get_4_points_from_gripper_pos_orient, infer_multitask_high_level_model, low_level_policy_infer, \
+    get_aloha_future_eef_poses_from_delta_actions
+from matplotlib import pyplot as plt
 
 def add_eef_pose(real_joints):
     eef_pose, eef_pose_se3 = forward_kinematics(ALOHA_CONFIGURATION, real_joints)
@@ -231,7 +237,8 @@ def control_loop(
     display_data=False,
     dataset: LeRobotDataset | None = None,
     events=None,
-    policy: PreTrainedPolicy = None,
+    policy=None,
+    # policy: PreTrainedPolicy = None,
     fps: int | None = None,
     single_task: str | None = None,
 ):
@@ -258,7 +265,7 @@ def control_loop(
     start_episode_t = time.perf_counter()
 
     # Controls starts, if policy is given it needs cleaning up
-    if policy is not None:
+    if policy is not None and has_method(policy, "reset"):
         policy.reset()
 
     while timestamp < control_time_s:
@@ -275,7 +282,7 @@ def control_loop(
                 observation["observation.right_eef_pose"] = add_eef_pose(observation['observation.state'])
             action = None
 
-            if policy is not None:
+            if policy is not None and type(policy) != dict:
                 # Pretty ugly, but moving this code inside the policy makes it uglier to visualize
                 # the goal_gripper_proj key.
 
@@ -371,16 +378,152 @@ def control_loop(
                 if robot.use_eef:
                     action["action.right_eef_pose"] = pred_action_eef
 
+            ### NOTE: write my own inference of the high and low-level policy
+            if type(policy) == dict:
+                high_level_policy, low_level_policy = policy['high_level'], policy['low_level']
+                action_queue = policy['action_queue']
+
+                # import pdb; pdb.set_trace()
+                if len(action_queue) == 0:
+                    ### NOTE: get the scene pcd from the depth camera
+                    depth_keys = ["observation.images.cam_azure_kinect_front.transformed_depth", "observation.images.cam_azure_kinect_back.transformed_depth"]
+                    # import pdb; pdb.set_trace()
+                    all_cam_depth_images = []
+                    for depth_key in depth_keys:
+                        depth = Image.fromarray(observation[depth_key].numpy()[:, :, 0])
+                        depth = np.asarray(depth)
+                        all_cam_depth_images.append(depth)
+                    
+                    # import pdb; pdb.set_trace()
+                    # fig, axes = plt.subplots(1, 2)
+                    # axes = axes.reshape(-1)
+                    # axes[0].imshow(all_cam_depth_images[0])    
+                    # axes[1].imshow(all_cam_depth_images[1])
+                    # plt.show()    
+                    # plt.close("all")
+                    
+                    # import pdb; pdb.set_trace()
+                    scene_pcd, scene_pcd_in_table_center = compute_pcd(all_cam_depth_images, num_points=4500)
+                  
+                    
+                    # import pdb; pdb.set_trace()
+                    ### NOTE: get the gripper 4 points
+                    right_eef_pose = observation['observation.right_eef_pose']
+                    cprint(f"current eef position: { right_eef_pose[6:9]}", "red")
+                    cprint(f"current eef position: { right_eef_pose[6:9]}", "red")
+                    cprint(f"current eef position: { right_eef_pose[6:9]}", "red")
+                    cprint(f"current eef position: { right_eef_pose[6:9]}", "red")
+                    eef_pos, eef_rot_6d, eef_gripper_width, eef_pos_robot_base, eef_rot_matrix_robot_base, eef_rot_6d_robot_base, eef_gripper_width_franka = get_gripper_4_points_from_sriram_data(right_eef_pose)
+                    agent_pos = np.array([*eef_pos_robot_base, *eef_rot_6d_robot_base, *eef_gripper_width_franka])
+                    eef_4_points = get_4_points_from_gripper_pos_orient(eef_pos_robot_base, eef_rot_matrix_robot_base, eef_gripper_width_franka)
+                    
+                    high_level_input_np = np.concatenate([scene_pcd, eef_4_points], axis=0)  # (N, 3)
+                    high_level_input = torch.from_numpy(high_level_input_np).float().unsqueeze(0).cuda()
+
+                    # import pdb; pdb.set_trace()
+                    with torch.no_grad():
+                        high_level_predict = infer_multitask_high_level_model(
+                            high_level_input, high_level_policy, policy['cat_embedding']
+                        )  # 1, 4, 3 ## torch tensor
+
+                   
+                    ### TODO: figure out how to use the history here
+                    # import pdb; pdb.set_trace()
+                    with torch.no_grad():
+                        low_level_action = low_level_policy_infer(
+                            torch.from_numpy(scene_pcd).float().to('cuda').unsqueeze(0).unsqueeze(1).repeat(1,2,1,1),
+                            torch.from_numpy(agent_pos).to('cuda').float().unsqueeze(0).unsqueeze(1).repeat(1,2,1), 
+                            high_level_predict.float().to('cuda').unsqueeze(1).repeat(1, 2, 1, 1),
+                            torch.from_numpy(eef_4_points).float().to('cuda').unsqueeze(0).unsqueeze(1).repeat(1,2,1,1),
+                            low_level_policy,
+                            cat_idx=policy['cat_idx']
+                        )
+
+                        low_level_action_np = low_level_action.cpu().numpy().reshape(-1, 10)
+                        delta_pos_mag = np.linalg.norm(low_level_action_np[:, :3], axis=1)
+                        print("delta pos magnitude is: ", delta_pos_mag)
+
+                    
+                    ### convert this low-level action back to the aloha eef pose
+                    ### TODO: note this max_relative_target thing
+                    # import pdb; pdb.set_trace()
+                    aloha_world_eef_pos, aloha_world_eef_orient_6d, aloha_gripper_widths, robot_base_eef_pos = get_aloha_future_eef_poses_from_delta_actions(
+                        low_level_action, 
+                        eef_pos_robot_base, 
+                        eef_rot_matrix_robot_base, 
+                        eef_gripper_width_franka
+                    )
+
+                    ### 3d plot the scene pcd
+                    fig = plt.figure(figsize=(10, 5))
+
+                    ax = fig.add_subplot(1, 2, 1, projection='3d')
+                    ax.scatter(scene_pcd[:,0], scene_pcd[:,1], scene_pcd[:,2], s=1, color='grey')
+                    ax.scatter(eef_4_points[:,0], eef_4_points[:,1], eef_4_points[:,2], s=50, color='green')
+                    high_level_predict_np = high_level_predict.cpu().numpy()[0]
+                    ax.scatter(high_level_predict_np[:,0], high_level_predict_np[:,1], high_level_predict_np[:,2], s=50, color='red')
+                    ax.plot(robot_base_eef_pos[:,0], robot_base_eef_pos[:,1], robot_base_eef_pos[:,2], color='blue')
+                    ax.set_xlabel("X")
+                    ax.set_ylabel("Y")
+                    ax.set_zlabel("Z")
+                    ax.scatter([0], [0], [0], color='red', s=100)
+
+                    ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+                    ax2.scatter(scene_pcd_in_table_center[:,0], scene_pcd_in_table_center[:,1], scene_pcd_in_table_center[:,2], s=1, color='grey')
+                    # ax.scatter(eef_4_points[:,0], eef_4_points[:,1], eef_4_points[:,2], s=50, color='green')
+                    # high_level_predict_np = high_level_predict.cpu().numpy()[0]
+                    # ax.scatter(high_level_predict_np[:,0], high_level_predict_np[:,1], high_level_predict_np[:,2], s=50, color='red')
+                    ax2.plot(aloha_world_eef_pos[:,0], aloha_world_eef_pos[:,1], aloha_world_eef_pos[:,2], color='blue')
+                    ax2.set_xlabel("X")
+                    ax2.set_ylabel("Y")
+                    ax2.set_zlabel("Z")
+                    ax2.scatter([0], [0], [0], color='red', s=100)
+
+                    plt.show()
+                    
+                    
+                    
+                    
+                    ### TODO: figure out what command is sent to the robot
+                    # import pdb; pdb.set_trace()
+                    all_aloha_eef_poses = []
+                    for  pos, orient_6d, gripper_width in zip(aloha_world_eef_pos, aloha_world_eef_orient_6d, aloha_gripper_widths):
+                        eef_pose = [*orient_6d, *pos, gripper_width]
+                        eef_pose = torch.tensor(eef_pose).float()
+                        all_aloha_eef_poses.append(eef_pose)
+
+                    # import pdb; pdb.set_trace()
+                    cprint(f"commanded future eef position: { aloha_world_eef_pos}", "green")
+
+
+                
+                    # import pdb; pdb.set_trace()
+                    robot_adapter = policy.get('robot_adapter', None)
+                    state = observation['observation.state']
+                    # import pdb; pdb.set_trace()
+                    aloha_joint_actions = [robot_adapter.transform_action(x, state) for x in all_aloha_eef_poses]
+                    for aja in aloha_joint_actions:
+                        action_queue.append(aja)
+                        
+                action_joint = action_queue.popleft()
+                # import pdb; pdb.set_trace()
+                action_joint = robot.send_action(action_joint)
+                action = {"action": action_joint}
+                if robot.use_eef:
+                    action["action.right_eef_pose"] = np.zeros(10, dtype=np.float32) ### TODO: record the true eef pose
+
+                
+               
         if dataset is not None:
             frame = {**observation, **action, "task": single_task}
             dataset.add_frame(frame)
 
         # TODO(Steven): This should be more general (for RemoteRobot instead of checking the name, but anyways it will change soon)
         if (display_data and not is_headless()) or (display_data and robot.robot_type.startswith("lekiwi")):
-            if action is not None:
-                for k, v in action.items():
-                    for i, vv in enumerate(v):
-                        rr.log(f"sent_{k}_{i}", rr.Scalar(vv.numpy()))
+            # if action is not None:
+            #     for k, v in action.items():
+            #         for i, vv in enumerate(v):
+            #             rr.log(f"sent_{k}_{i}", rr.Scalar(vv.numpy()))
 
             image_keys = [key for key in observation if "image" in key]
             for key in image_keys:

lerobot/scripts/control_robot.py
@@ -261,7 +261,7 @@ def record(
         sanity_check_dataset_robot_compatibility(dataset, robot, cfg.fps, cfg.video)
     else:
         # Create empty dataset or load existing saved episodes
-        sanity_check_dataset_name(cfg.repo_id, cfg.policy)
+        # sanity_check_dataset_name(cfg.repo_id, cfg.policy)
 
         if cfg.policy is not None and cfg.policy.enable_goal_conditioning:
             with open(cfg.policy.hl_calibration_json) as f:
@@ -286,12 +286,34 @@ def record(
         )
 
     # Load pretrained policy
-    policy = None if cfg.policy is None else make_policy(cfg.policy, ds_meta=dataset.meta)
+    exp_dir = "/data/yufei/lerobot/data/low-level-ckpt/1204_finetune_ours_sriram_plate_combine_2_step_train_longer_keep_old_normalizer" 
+    checkpoint_name = "epoch-300.ckpt"
+    from lerobot.scripts.yufei_policy_utils import load_low_level_policy, load_multitask_high_level_model
+    low_level_policy = load_low_level_policy(exp_dir, checkpoint_name)
+    model_path = "/data/yufei/lerobot/data/high-level-ckpt/2025-12-04fine_tune_our_on_sriram_lr_decay/model_15001.pth"
+    high_level_policy, _ = load_multitask_high_level_model(model_path)
+    import torch
+    siglip_text_features = torch.load(os.path.join(os.environ['PROJECT_DIR'], "siglip_text_features_w_pick_and_place.pt"))
+    siglip_text_features = siglip_text_features['values']
+    from lerobot.common.policies.robot_adapters import AlohaAdapter
+    robot_adapter = AlohaAdapter(action_space="right_eef")
+    from collections import deque
+    cat_idx = 13
+    policy = {
+        "high_level": high_level_policy,
+        "low_level": low_level_policy,
+        "cat_embedding": siglip_text_features[cat_idx].float().to("cuda"),
+        "cat_idx": cat_idx,
+        "robot_adapter": robot_adapter,
+        "action_queue": deque(),
+    }
+    # import pdb; pdb.set_trace()
 
     if not robot.is_connected:
         robot.connect()
 
     listener, events = init_keyboard_listener()
+    # import pdb; pdb.set_trace()
 
     # Execute a few seconds without recording to:
     # 1. teleoperate the robot to move it in starting position if no policy provided,
@@ -300,15 +322,30 @@ def record(
     enable_teleoperation = policy is None
     log_say("Warmup record", cfg.play_sounds)
     warmup_record(robot, events, enable_teleoperation, cfg.warmup_time_s, cfg.display_data, cfg.fps)
+    # import pdb; pdb.set_trace()
 
     if has_method(robot, "teleop_safety_stop"):
         robot.teleop_safety_stop()
 
     recorded_episodes = 0
+    # import pdb; pdb.set_trace()
     while True:
         if recorded_episodes >= cfg.num_episodes:
             break
 
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        print("going to record episode!!!!!!!!!!!!!!!")
+        # import pdb; pdb.set_trace()
         log_say(f"Recording episode {dataset.num_episodes}", cfg.play_sounds)
         record_episode(
             robot=robot,
@@ -417,13 +454,33 @@ def _init_rerun(control_config: ControlConfig, session_name: str = "lerobot_cont
             memory_limit = os.getenv("LEROBOT_RERUN_MEMORY_LIMIT", "10%")
             rr.spawn(memory_limit=memory_limit)
 
-
+"""
+python lerobot/scripts/control_robot.py 
+    --robot.type=aloha 
+    --control.type=record 
+    --control.fps=30 
+    --control.single_task="Grasp mug and place it on the table." 
+    --control.repo_id=sriramsk/eval_aloha_eef_rgb_0709_heatmapGoal 
+    --control.num_episodes=1 
+    --control.reset_time_s=5 
+    --control.warmup_time_s=3 
+    --robot.cameras='{"cam_azure_kinect": {"type": "azurekinect", "device_id": 0, "fps": 30, "width": 1280, "height": 720, "use_transformed_depth": true}}' 
+    --robot.use_eef=true 
+    --control.push_to_hub=false 
+    --control.policy.path=outputs/train/diffPo_aloha_eef_rgb_0709_heatmapGoal/checkpoints/last/pretrained_model/ 
+    --control.display_data=true 
+    --control.episode_time_s=120
+"""
 @parser.wrap()
 def control_robot(cfg: ControlPipelineConfig):
+    # import pdb; pdb.set_trace()
     init_logging()
+    # import pdb; pdb.set_trace()
     logging.info(pformat(asdict(cfg)))
 
+    # import pdb; pdb.set_trace()
     robot = make_robot_from_config(cfg.robot)
+    # import pdb; pdb.set_trace()
 
     # TODO(Steven): Blueprint for fixed window size
 
@@ -433,7 +490,9 @@ def control_robot(cfg: ControlPipelineConfig):
         _init_rerun(control_config=cfg.control, session_name="lerobot_control_loop_teleop")
         teleoperate(robot, cfg.control)
     elif isinstance(cfg.control, RecordControlConfig):
+        # import pdb; pdb.set_trace()
         _init_rerun(control_config=cfg.control, session_name="lerobot_control_loop_record")
+        # import pdb; pdb.set_trace()
         record(robot, cfg.control)
     elif isinstance(cfg.control, ReplayControlConfig):
         replay(robot, cfg.control)

scripts/convert_dataset.py
@@ -39,8 +39,20 @@ def R_y(deg):
         [-s, 0,  c],
     ])
 
+def R_z(deg):
+    theta = np.deg2rad(deg)
+    c, s = np.cos(theta), np.sin(theta)
+    return np.array([
+        [ c, s,  0],
+        [ -s, c,  0],
+        [0, 0,  1],
+    ])
+
+
 # Fixed transform: viper frame → franka frame
-R_fv = R_y(-90.0) 
+# R_fv = R_y(-90.0) 
+R_vf_y = R_y(90.0)
+R_vf_z = R_z(180)
 
 def transform_from_table_center_to_robot_base(xyz_in_table_center):
     # xyz_robot_base = xyz_in_table_center - robot_base_in_table_center_frame
@@ -196,7 +208,8 @@ def get_gripper_4_points_from_sriram_data(eef_pose_from_sriram):
     right_eef_pose = eef_pose_from_sriram
     eef_rot_6d = right_eef_pose[:6]
     eef_rot_matrix = rotation_6d_to_matrix(eef_rot_6d[None, :]).squeeze().cpu().numpy()  # (3, 3)
-    eef_rot_matrix_franka_eef_coordinate = R_fv @ eef_rot_matrix
+    # eef_rot_matrix_franka_eef_coordinate = R_fv @ eef_rot_matrix
+    eef_rot_matrix_franka_eef_coordinate = eef_rot_matrix @ R_vf_y @ R_vf_z
     eef_rot_matrix_robot_base = R_world_to_robot @ eef_rot_matrix_franka_eef_coordinate
     # eef_rot_matrix_robot_base = eef_rot_matrix # @ R_world_to_robot 
     eef_rot_6d_robot_base = matrix_to_rotation_6d(torch.from_numpy(eef_rot_matrix_robot_base)[None, :]).squeeze().cpu().numpy()
@@ -319,6 +332,7 @@ for cam_name, cam_cfg in cameras.items():
     print(f"Loaded extrinsics for cam_name: {cam_name}: {T}")
 
 num_points = 4500
+# num_points = 30000
 max_depth = 1.5
 
 for traj_idx in range(dataset.num_episodes):
@@ -343,6 +357,7 @@ for traj_idx in range(dataset.num_episodes):
     # all_gripper_finger_widths = [x[-1].item() for x in all_eef_pose]
     
     for t_idx in range(from_idx, to_idx):
+    # for t_idx in range(from_idx + 100, to_idx):
         
         print(f"Processing traj {traj_idx}, frame_idx {t_idx - from_idx} / {traj_len}")
         
@@ -420,6 +435,30 @@ for traj_idx in range(dataset.num_episodes):
         all_pcd_in_world = np.concatenate(all_pcd_in_world, axis=0)  # (num_cams * num_points, 3)
         end = time.time()
         # print("time for getting scene pcd from multiview depth: ", end - beg)
+
+
+        # pcd1 = o3d.geometry.PointCloud()
+        # pcd1.points = o3d.utility.Vector3dVector(all_pcd_in_world)
+        # pcd1.paint_uniform_color([1, 0, 0])   # Red
+
+        # geometries = [pcd1]
+
+        # # --- Create spheres for pcd2 ---
+        # sphere_radius = 0.01
+        # sphere_resolution = 10
+
+        # for p in eef_4_points:
+        #     sphere = o3d.geometry.TriangleMesh.create_sphere(
+        #         radius=sphere_radius, 
+        #         resolution=sphere_resolution
+        #     )
+        #     sphere.paint_uniform_color([0, 0, 1])  # red spheres
+        #     sphere.translate(p)                    # move sphere to point location
+        #     geometries.append(sphere)
+
+        # # Visualize
+        # o3d.visualization.draw_geometries(geometries)
+
         
         ### perform fps here
         beg = time.time()
@@ -481,8 +520,8 @@ for traj_idx in range(dataset.num_episodes):
         # print("time for getting goal gripper pcd: ", end - beg)
         
         # %matplotlib widget
-        from matplotlib import pyplot as plt
-        # plt.close("all")
+        # from matplotlib import pyplot as plt
+        # # plt.close("all")
         # ax = plt.subplot(projection='3d')
         # ax.scatter(all_pcd_in_world[:, 0], all_pcd_in_world[:, 1], all_pcd_in_world[:, 2], s=1, color='blue')
         # ax.scatter(goal_x_in_robot_base[:, 0], goal_x_in_robot_base[:, 1], goal_x_in_robot_base[:, 2], s=1, color='blue')
@@ -530,7 +569,7 @@ for traj_idx in range(dataset.num_episodes):
     action_arrays, traj_scene_pcd, traj_eef_pose, traj_gripper_pcd, traj_goal_gripper_pcd = \
         extract_actions(traj_eef_pose, traj_scene_pcd, traj_gripper_pcd, traj_goal_gripper_pcd, combine_action_steps=2)
 
-    data_dir = "/data/yufei/lerobot/data"
+    data_dir = "/data/yufei/lerobot/data/plate_new_rot"
     traj_dir = os.path.join(data_dir, f"traj_{traj_idx:04d}")
     if not os.path.exists(traj_dir):
         os.makedirs(traj_dir)